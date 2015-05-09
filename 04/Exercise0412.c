61110ead286421c2996edaf4fb967b8a611193ef
lianghao13 <lianghao13@msn.com>
2015-05-10 03:04:23 +0800

    test modify

--- 04/Exercise0412.c
+++ 04/Exercise0412.c
@@ -6,12 +6,25 @@
 
 int main(void){
     int num, i;
+    int j = 0;
     
     do {
         printf("请输入一个正整数:");    scanf("%d", &num);
         if (num <= 0)
             puts("\a请输入正整数。");
     } while (num <= 0);
+
+    for (i = 0; i < num; ++i) {
+        if (j < 9) {
+            printf("%d", ++j);
+        }
+        else {
+            j = 0;
+            printf("%d", j);
+        }
+    }
+
+    putchar('\n');
 
     return (0);
 }
